c49c438dd550b887ca048bd7e3921ca3a5dd814e
Yahya BENABDALLAH <Hyrkul@outlook.com>
2020-07-14 02:06:33 +0200

    CRUD voiture, Libelle a La place du ID

---
+++ Examen_2019/Examen_2019/Controllers/VoituresController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Examen_2019.Models;
+
+namespace Examen_2019.Controllers
+{
+    public class VoituresController : Controller
+    {
+        private readonly MyContext _context;
+
+        public VoituresController(MyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Voitures
+        public async Task<IActionResult> Index()
+        {
+            var myContext = _context.Voitures.Include(v => v.marque);
+            return View(await myContext.ToListAsync());
+        }
+
+        // GET: Voitures/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var voiture = await _context.Voitures
+                .Include(v => v.marque)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (voiture == null)
+            {
+                return NotFound();
+            }
+
+            return View(voiture);
+        }
+
+        // GET: Voitures/Create
+        public IActionResult Create()
+        {
+            ViewData["MarqueId"] = new SelectList(_context.Marques, "Id", "Libelle"); // Question changement de l'affichage id ==> Libelle
+            return View();
+        }
+
+        // POST: Voitures/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Matricule,Nbr_portes,Nbr_places,Photo_1,Couleur,MarqueId")] Voiture voiture)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(voiture);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MarqueId"] = new SelectList(_context.Marques, "Id", "Id", voiture.MarqueId);
+            return View(voiture);
+        }
+
+        // GET: Voitures/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var voiture = await _context.Voitures.FindAsync(id);
+            if (voiture == null)
+            {
+                return NotFound();
+            }
+            ViewData["MarqueId"] = new SelectList(_context.Marques, "Id", "Id", voiture.MarqueId);
+            return View(voiture);
+        }
+
+        // POST: Voitures/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Matricule,Nbr_portes,Nbr_places,Photo_1,Couleur,MarqueId")] Voiture voiture)
+        {
+            if (id != voiture.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(voiture);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VoitureExists(voiture.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MarqueId"] = new SelectList(_context.Marques, "Id", "Id", voiture.MarqueId);
+            return View(voiture);
+        }
+
+        // GET: Voitures/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var voiture = await _context.Voitures
+                .Include(v => v.marque)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (voiture == null)
+            {
+                return NotFound();
+            }
+
+            return View(voiture);
+        }
+
+        // POST: Voitures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var voiture = await _context.Voitures.FindAsync(id);
+            _context.Voitures.Remove(voiture);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VoitureExists(int id)
+        {
+            return _context.Voitures.Any(e => e.Id == id);
+        }
+    }
+}
